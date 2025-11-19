6c498adba8aaa8b3d20853cf18f785a13d50c995
Felix Almqvist <kiwibirdi@hotmail.com>
2025-11-19 15:30:50 +0100

    försöker lära mig

--- cypress/e2e/formFill.cy.js
+++ cypress/e2e/formFill.cy.js
@@ -1,5 +1,32 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Fills in the form with correct & minimal info', () => {
+  
+  it('Fills in the form', () => {
     cy.visit('https://demoqa.com/automation-practice-form')
+    //Name
+    cy.get('#firstName').type('tmp')
+    cy.get('#lastName').type('pmt')
+    //email
+    cy.get('#userEmail').type('hej@tmp.se')
+    //gender
+    cy.contains('Male').click()
+    //phone number
+    cy.get('#userNumber').type('0123456789')
+    //DOB 
+
+    //Hobbies
+
+    //Picture
+
+    //Adress
+
+    //State
+
+    //City
+
+    //Submit & assert
+    cy.get('#submit').click()
+    cy.get('.modal-header').should('be.visible')
   })
+
+  
 })
